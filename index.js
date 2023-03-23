fab3e2fe973b1d27d2e38119adb4b1b5b2601356
Rawan <r.k096@hotmail.com>
2023-03-23 21:35:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(x){
+    return Math.abs(42 - x)
+}
+
+function  distanceFromHqInFeet(x){
+    return distanceFromHqInBlocks(x) * 264
+}
+
+function  distanceTravelledInFeet(x, y){
+    let dis = Math.abs(x-y) 
+    return dis * 264
+}
+
+function  calculatesFarePrice(x, y){
+    let dis = distanceTravelledInFeet(x, y) 
+    if(dis < 400 )
+        return 0
+    else if(dis > 400 && dis < 2000)
+        return (dis - 400) * 0.02
+    else if(dis > 2000 && dis < 2500)    
+        return 25
+    else if (dis > 2500) 
+        return 'cannot travel that far'   
+
+}
+
+console.log(calculatesFarePrice(34, 32))
